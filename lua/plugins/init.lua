075726d580bdbeadfdbcd5fcdf3a7db7ea7f1334
hkhrithik007 <hkhrithik@HKs-MacBook-Air.local>
2024-06-24 23:18:17 +0530

    minor optimzation

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -494,7 +494,7 @@ return {
   },
   {
     "willothy/nvim-cokeline",
-    event = "VeryLazy",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "nvim-lua/plenary.nvim", -- Required for v0.4.0+
       "nvim-tree/nvim-web-devicons", -- If you want devicons
